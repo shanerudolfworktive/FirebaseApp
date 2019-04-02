1aa4e1aa857bd1ad1da90fb1648dc728e6a3868b
Xiaoxie Fu <xiaoxiefu@xiaoxies-mbp.attlocal.net>
2019-04-02 08:37:45 -0700

    add list view

--- app/src/main/java/com/example/fiarebaseapp/MainFragment.kt
+++ app/src/main/java/com/example/fiarebaseapp/MainFragment.kt
@@ -25,14 +25,4 @@ class MainFragment : BaseFragment() {
         super.onCreate(savedInstanceState)
         findNavController().navigate(R.id.action_mainFragment_to_productListFragment)
     }
-
-    override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_main, container, false)
-    }
-
-
 }
--- app/src/main/java/com/example/fiarebaseapp/views/fragments/ProductListFragment.kt
+++ app/src/main/java/com/example/fiarebaseapp/views/fragments/ProductListFragment.kt
@@ -1,4 +1,4 @@
-package com.example.fiarebaseapp
+package com.example.fiarebaseapp.views.fragments
 
 
 import android.os.Bundle
@@ -6,6 +6,7 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import com.example.fiarebaseapp.R
 
 
 // TODO: Rename parameter arguments, choose names that match
