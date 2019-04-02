ec0cd08adc52c75fb7bf2e9a89b209736eff6017
Xiaoxie Fu <xiaoxiefu@xiaoxies-mbp.attlocal.net>
2019-04-02 16:25:30 -0700

    style a little bit in details view

--- app/src/main/java/com/example/fiarebaseapp/ProductDetailFragment.kt
+++ app/src/main/java/com/example/fiarebaseapp/ProductDetailFragment.kt
@@ -1,17 +1,22 @@
 package com.example.fiarebaseapp
 
 
+import android.os.Build
 import android.os.Bundle
+import android.text.Html
 import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageView
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProviders
 import androidx.navigation.fragment.navArgs
 import com.example.fiarebaseapp.viewmodels.ProductDetailViewModel
 import com.google.gson.Gson
+import com.squareup.picasso.Picasso
+import kotlinx.android.synthetic.main.fragment_product_detail.*
 
 
 // TODO: Rename parameter arguments, choose names that match
@@ -31,9 +36,20 @@ class ProductDetailFragment : Fragment() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         viewmodel = ViewModelProviders.of(activity!!).get(ProductDetailViewModel::class.java)
-
         viewmodel.productModel.observe(this, Observer {
-            Log.e("testing", "product=" + Gson().toJson(it))
+            Picasso.get().load(url).fit().placeholder(R.drawable.default_image).error(R.drawable.default_image)
+                .into(productImageView)
+            nameTextView.text = it.productName
+            priceTextView.text = it.price
+            instoreTextView.text = "in store: " + it.inStock
+            ratingTextView.text = "rating: " + it.reviewRating
+            ratingCountTextView.text = "review count: " + it.reviewCount
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.N) {
+                descriptionTextView.text = Html.fromHtml(it.longDescription, Html.FROM_HTML_MODE_LEGACY)
+            } else {
+                descriptionTextView.text = Html.fromHtml(it.longDescription)
+            }
+
         })
 
         viewmodel.productById(args.productId!!)
@@ -43,10 +59,12 @@ class ProductDetailFragment : Fragment() {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-
-        Log.e("testing", "id=" + args.productId)
         return inflater.inflate(R.layout.fragment_product_detail, container, false)
     }
 
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        Picasso.get().load(R.drawable.default_image).into(productImageView)
+    }
 
 }
