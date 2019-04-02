fda05c981e08b455f276e9fdd7555f7aad6aeefa
Xiaoxie Fu <xiaoxiefu@xiaoxies-mbp.attlocal.net>
2019-04-02 08:15:12 -0700

    add navigation stack

---
+++ app/src/main/java/com/example/fiarebaseapp/views/fragments.kt
@@ -0,0 +1,2 @@
+package com.example.fiarebaseapp.views
+
---
+++ app/src/main/java/com/example/fiarebaseapp/views/fragments/BaseFragment.kt
@@ -0,0 +1,6 @@
+package com.example.fiarebaseapp.views.fragments
+
+import androidx.fragment.app.Fragment
+
+open class BaseFragment : Fragment(){
+}
